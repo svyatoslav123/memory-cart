ebe4c525a26b75585f8a2ca96879815ce0a2f5e4
svyatoslav123 <151392816+svyatoslav123@users.noreply.github.com>
2024-01-13 16:38:26 +0200

    e

--- menu_window.py
+++ menu_window.py
@@ -8,8 +8,15 @@ def menu_window():
     window = QDialog()
     quest_lbl = QLabel("Введіть запитання")
     right_ans_lbl = QLabel("Введіть правильну")
+    wrong_ans_lbl1 = QLabel("Введіть неправильну1")
+    wrong_ans_lbl2 = QLabel("Введіть неправильну2")
+    wrong_ans_lbl3 = QLabel("Введіть неправильну3")
+
     quest_edit = QLineEdit()
-    right_ans_lbl = QLineEdit()
+    right_ans_input = QLineEdit()
+    wrong_ans_input1 = QLineEdit()
+    wrong_ans_input2 = QLineEdit()
+    wrong_ans_input3 = QLineEdit()
     add_quest_btn = QPushButton()
 
 
@@ -23,6 +30,26 @@ def menu_window():
     h1.addWidget(quest_lbl)
     h1.addWidget(quest_edit)
     main_line.addLayout(h1)
+    h2 = QHBoxLayout()
+
+    h2.addWidget(right_ans_lbl)
+    h2.addWidget(right_ans_input)
+    main_line.addLayout(h2)
+
+    h3 = QHBoxLayout()
+    h3.addWidget(wrong_ans_lbl1)
+    h3.addWidget(wrong_ans_input1)
+    main_line.addLayout(h3)
+
+    h4 = QHBoxLayout()
+    h4.addWidget(wrong_ans_lbl2)
+    h4.addWidget(wrong_ans_input2)
+    main_line.addLayout(h4)
+
+    h5 = QHBoxLayout()
+    h5.addWidget(wrong_ans_lbl3)
+    h5.addWidget(wrong_ans_input3)
+    main_line.addLayout(h5)
     def add_quest_func():
         a = {
             "Запитання" : quest_edit.text(),
